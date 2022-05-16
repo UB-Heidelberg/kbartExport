5dfafa781f2047d27d031dfee4370bb28bec9242
Christoph Otte <otte@ub.uni-heidelberg.de>
2022-05-16 11:01:06 +0200

    Copyrights updated

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 /**
  * @file plugins/generic/kbartExport/index.php
  *
+ * Copyright (c) 2014-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
  * Copyright (c) 2022 Heidelberg University
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
